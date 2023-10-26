dd8fa727a65d396e9bc4a73822659f6cbb409d1c
Dzmitry Liashko <dzliashko@gmail.com>
2023-10-26 08:11:20 +0300

    Feat: stage 2 finished

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,8 +1,24 @@
 package lastpencil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("||||||||");
-        System.out.println("Your turn! ");
+        Scanner scanner = new Scanner(System.in);
+        int pencilsQty;
+        String name;
+        System.out.println("How many pencils would you like to use:");
+        pencilsQty = scanner.nextInt();
+        System.out.println("Who will be the first (John, Jack):");
+        name = scanner.next();
+        printPencils(pencilsQty);
+        System.out.printf("%s is going first!", name);
+    }
+
+    private static void printPencils(int qty) {
+        for (int i = 0; i < qty; i++) {
+            System.out.print("|");
+        }
+        System.out.println();
     }
 }
